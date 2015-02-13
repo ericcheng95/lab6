db7445154b5197267451427893b0043d788a2088
Eric Cheng <ericcheng95@gmail.com>
2015-02-12 17:26:05 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,7 +27,17 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	//var proj = '#project[' + idNumber + '].details';
+	var proj = $(this).closest('.project');
+	var detail = $(this).next();
+
+	$.get("/project/" + idNumber, function(result){
+		$(detail).html(result.summary);
+		console.log(result);
+	});
 }
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +45,13 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", function(result){
+		var colors = result.colors.hex;
+		console.log(result.colors.hex);
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1,h2,h3,h4,h5,h5').css('color', colors[2]);
+		$('p').css('color',colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
